810ed454bf2fc51d7e29b539cd4c231cf4c3dc3f
kanno <812137533@qq.com>
2022-04-09 22:35:03 +0800

    feat: support ssrRef

--- src/environment.ts
+++ src/environment.ts
@@ -1,3 +1,5 @@
+import { getSSRSymbleStatus } from './un/context'
+
 export enum NodeEnv {
   DEV = 'development',
   PROD = 'production'
@@ -7,4 +9,10 @@ export const NODE_ENV = process.env.NODE_ENV as NodeEnv
 export const isDev = process.env.NODE_ENV === NodeEnv.DEV
 export const isProd = process.env.NODE_ENV === NodeEnv.PROD
 
-export const isSSR = () => typeof window !== 'undefined'
+export const isSSR = typeof window !== 'undefined' || getSSRSymbleStatus()
+export const isSPA = !isSSR
+
+// vite runtime env
+export const isServer = import.meta.env.SSR
+
+export const isClient = !isServer
